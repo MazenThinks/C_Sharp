0f18aed71ff3e5b29f2834de92e48bab99d9f7bf
MazenThinks <mazenmagdy100.mm@gmail.com>
2023-05-21 19:12:20 +0300

    Write a C# program to find factorial of a number using for.

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Factroial
+{
+    public int x;
+
+    public Factroial (int num)
+    {
+        x = num;
+    }
+
+    public void FacCalc()
+    {
+        int result = 1;
+
+        for(int i = 1; i <= x; i++)
+        {
+            result *= i;
+        }
+
+        Console.WriteLine("The factorial of {0} is {1}", x, result);
+        
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Enter the number u want the factorial for: ");
+        int num = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        Factroial factorial = new Factroial(num);
+        factorial.FacCalc();
+    }
+}
+
+
