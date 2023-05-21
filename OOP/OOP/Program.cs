323a7f138114635dfafe2592b8e0b63086bf1bf3
MazenThinks <mazenmagdy100.mm@gmail.com>
2023-05-21 19:50:43 +0300

    calculating area of square, rectangle, triangle and circle

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -1,37 +1,54 @@
 ﻿using System;
 
-public class number
+namespace OOP;
+
+public class shape
 {
-    public int x;
+    public int result;
+    public double pi = 3.14;
 
-    public number (int num)
+    // Square
+    public void Area(int side)
     {
-        x = num;
+        result = side * side;
+        Console.WriteLine("The area of the sqaure is: {0}", result);
     }
 
-    public void numba()
+    // Rectangle
+    public void Area(int length, int width)
     {
-        
-        for(int i = 1; i <= x; i++)
-        {
-            Console.WriteLine(i);
-        }
-        
+        result = length * width;
+        Console.WriteLine("The area of the rectangle is: {0}", result);
     }
+
+    // Triangle
+    public void Area(double basee, double height)
+    {
+        double result = (basee * height) / 2;
+        Console.WriteLine("The area of the triangle is: {0}", result);
+    }
+
+    // Circle
+    public void Area(double radius)
+    {
+        double result = pi * radius * radius;
+        Console.WriteLine("The area of the circle is: {0}", result);
+    }
+
 }
+
+
 
 
 class Program
 {
     static void Main()
     {
-
-        Console.Write("Enter a number: ");
-        int num = int.Parse(Console.ReadLine());
-        Console.WriteLine();
-
-        number factorial = new number(num);
-        factorial.numba();
+        shape area = new shape();
+        area.Area(10); // Square
+        area.Area(15, 30); // Rectangle
+        area.Area(15.7); // Circle
+        area.Area(17.8, 22.4); // Triangle
     }
 }
 
